c17b4c2edfa7c8f06bccfaccd3e9f1a61b73b51f
mira-rawr <137906015+mira-rawr@users.noreply.github.com>
2023-07-14 04:35:13 +0800

    Add files via upload

---
+++ DreamGoMRS 2/DreamGoMRS/client/src/configs/theme.configs.js
@@ -0,0 +1,50 @@
+import { createTheme } from "@mui/material/styles";
+import { colors } from "@mui/material";
+
+export const themeModes = {
+  dark: "dark",
+  light: "light"
+};
+
+const themeConfigs = {
+  custom: ({ mode }) => {
+    const customPalette = mode === themeModes.dark ? {
+      primary: {
+        main: "#01CCFE",
+        contrastText: "#ffffff"
+      },
+      secondary: {
+        main: "#D9D9D9",
+        contrastText: "#ffffff"
+      },
+      background: {
+        default: "#000000",
+        paper: "#131313"
+      }
+    } : {
+      primary: {
+        main: "#01CCFE"
+      },
+      secondary: {
+        main: "#D9D9D9"
+      },
+      background: {
+        default: colors.grey["100"],
+      }
+    };
+
+    return createTheme({
+      palette: {
+        mode,
+        ...customPalette
+      },
+      components: {
+        MuiButton: {
+          defaultProps: { disableElevation: true }
+        }
+      }
+    });
+  }
+};
+
+export default themeConfigs;
